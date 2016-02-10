f5743614d6cbf442ec3fbb5d0296976aca9bec06
Michelle Melton <meltonml@appstate.edu>
2016-02-09 21:28:54 -0500

    Adding plugin component to version file

--- version.php
+++ version.php
@@ -24,5 +24,6 @@
    * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
    */
 
+  $plugin->component = 'block_course_evals';
   $plugin->version = 2015092900;
   $plugin->requires = 2010112400;
